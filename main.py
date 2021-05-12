8e7ff46acd51bb702e028c79019b371e66365044
NikoriakViktot <83222767+NikoriakViktot@users.noreply.github.com>
2021-05-12 19:35:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,33 @@ class Person:
 
 
     def talk(self, person):
-        print ('Привіт, мене ' + self.name + self.surname + "мені" + self.age)
+       class Person:
+
+    def __init__(self, name, surname, age, ):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+
+    #def tolk(self, person):
+        print ('Привіт, мене звати  ' + self.name  + ' ' + self.surname + "  " + "мені" + " " + self.age)
+     #   return()
+    
+
+
+
+person = Person(
+    "Віктор",
+    "Нікоряк",
+    "39 років"
+
+)
+
+
+
+# Task2
+
+        return()
 
     
 
@@ -23,7 +49,7 @@ person = Person(
     "39 років"
 
 )
-print(talk)
+
 
 
 # Task2
