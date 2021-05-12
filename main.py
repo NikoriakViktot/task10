2618ddd5152ce6f55f2f235a1787974f2d285112
NikoriakViktot <83222767+NikoriakViktot@users.noreply.github.com>
2021-05-12 20:06:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
     # Task1
 
+
 class Person:
 
     def __init__(self, name, surname, age, ):
@@ -10,49 +11,24 @@ class Person:
         self.age = age
 
 
-    def talk(self, person):
-       class Person:
-
-    def __init__(self, name, surname, age, ):
-        self.name = name
-        self.surname = surname
-        self.age = age
-
-
-    #def talk(self, person):
+    def talk(self):
         print ('Привіт, мене звати  ' + self.name  + ' ' + self.surname + "  " + "мені" + " " + self.age)
-     #   return()
-    
-
-
-
+   
 person = Person(
     "Віктор",
     "Нікоряк",
     "39 років"
 
 )
-
-
-
-# Task2
-
-        return()
-
-    
-
-
-
-person = Person(
-    "Віктор",
-    "Нікоряк",
-    "39 років"
-
-)
-
+person.talk()
 
 
 # Task2
+
+
+
+
+
 
 
 
